311c6ab5f8673c61ae544d28c9a7c72b534a1791
Philippe Waty <philippe.waty@medsoc.be>
2023-10-17 10:25:03 +0200

    Clean using

--- UDBA/Test/Program.cs
+++ UDBA/Test/Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UDBA;
 
 namespace Test
