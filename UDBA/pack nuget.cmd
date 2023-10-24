8a9d0acfd24338001e00cdb8bcfa56ead5c985f7
Philippe Waty <47305204+philippewaty@users.noreply.github.com>
2023-10-24 12:12:40 +0200

    Changed version to 1.0.1

--- UDBA/pack nuget.cmd
+++ UDBA/pack nuget.cmd
@@ -1,7 +1,5 @@
 if not exist NuGet mkdir NuGet
 
-del /Q NuGet\*.*
-
 NuGet.exe pack UDBA.nuspec -OutputDirectory NuGet -IncludeReferencedProjects -Properties Configuration=Release;Platform=AnyCPU -Build
 
 rem pause
